c6b9360a1830effb825f64495055ba9b3be7e0d5
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-15 15:22:02 +0300

    THe word frequency

---
+++ word_frequency.py
@@ -0,0 +1,16 @@
+
+import re
+
+def word_frequency(sentence):
+    words = re.findall(r'\b\w+\b', sentence.lower())
+    frequency = {}
+
+    for word in words:
+        frequency[word] = frequency.get(word, 0) + 1
+
+    return frequency
+
+# Test the function
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
